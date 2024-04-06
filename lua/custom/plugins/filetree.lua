86ddfe2e50dbe654a713a445c2f5abba70eaac8b
scoo73r <scorb1@gmail.com>
2024-04-05 19:36:36 -0500

    test

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -62,6 +62,21 @@ return {
           use_git_status_colors = true,
           highlight = 'NeoTreeFileName',
         },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted = '✖', -- this can only be used in the git_status source
+            renamed = '󰁕', -- this can only be used in the git_status source
+            -- Status type
+            untracked = '',
+            ignored = '',
+            unstaged = '󰄱',
+            staged = '',
+            conflict = '',
+          },
+        },
         -- If you don't want to use these columns, you can set `enabled = false` for each of them individually
         file_size = {
           enabled = true,
@@ -174,7 +189,7 @@ return {
           },
         },
         follow_current_file = {
-          enabled = true, -- This will find and focus the file in the active buffer every time
+          enabled = false, -- This will find and focus the file in the active buffer every time
           --               -- the current file is changed while the tree is open.
           leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
         },
